b7697055f08bff970a70af4399cd60e6490da15c
Nitigya Kapoor <kapoornitigya@gmail.com>
2020-07-08 19:39:57 +0530

    single threaded , listening to tcp streams

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let html_filepath = "/Users/nitigyakapoor/rust-test/Rust-Server/src/hello.html";
+    let error_filepath = "/Users/nitigyakapoor/rust-test/Rust-Server/src/404.html";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", html_filepath)
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", html_filepath)
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", error_filepath)
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
